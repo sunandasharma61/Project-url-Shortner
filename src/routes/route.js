b4b8681f64b8c0d3995df2ff6dcf4fe2ee8744bf
sunandasharma61 <sunandasharma60101@gmail.com>
2022-12-30 20:30:39 +0530

    first push

---
+++ src/routes/route.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+const router = Router()
+const urlController = require('../controller/urlController')
+
+router.post('/url/shorten', urlController.createShortUrl )
+
+router.get('/:urlCode', urlController.redirectShortUrl )
+
+router.all("/*", (req, res)=>{
+    return res.status(404).send({status:false, message:" Invalid HTTP request "})
+})
+
+module.exports = router
